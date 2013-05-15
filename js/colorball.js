b86eb9e0aced83561063d93068873296fecd86ea
Dan Ross <ross9885@gmail.com>
2013-05-15 15:10:27 -0700

    Using quaternion rotation to slide edge vertices down the sphere surface.

--- js/colorball.js
+++ js/colorball.js
@@ -27,8 +27,7 @@ define(function(require) {
 			The coordinates of the plane must be in the same space as geometry.vertices.
 			*/
 			
-			var i;
-			var vertex;
+			var i, j, face, vertex, verticesPerFace;
 			var upperVertices = []; // I wish this was a set.
 			geometry.faces = originalFaces;
 
@@ -38,17 +37,18 @@ define(function(require) {
 					upperVertices.push(vertex);
 			}
 
+			// Remove faces that are above the plane.
 			var remainingFaces = [];
 			var intersectingEdges = [];
 			for(i = 0; i < geometry.faces.length; i++) {
-				var face = geometry.faces[i];
-				var verticesPerFace = (face instanceof THREE.Face3)? 3: 4;
+				face = geometry.faces[i];
+				verticesPerFace = (face instanceof THREE.Face3)? 3: 4;
 				var isFaceAbovePlane = true;
 
 				// Find faces to remove and find edges that intersect the plane.
 				var lastVertex;
 				var isLastVertexAbovePlane;
-				for(var j = 0; j < verticesPerFace; j++) {
+				for(j = 0; j < verticesPerFace; j++) {
 					vertex = geometry.vertices[face[faceIndices[j]]];
 					if(plane.distanceToPoint(vertex) >= 0) {
 						isLastVertexAbovePlane = true;
@@ -65,6 +65,27 @@ define(function(require) {
 					remainingFaces.push(face);
 			}
 			geometry.faces = remainingFaces;
+
+			// Slide vertices that are above the plane down to the plane.
+			// Find all unique vertices in the intersecting edges that are above the plane.
+			var edgeVerts = {};
+			for(i = 0; i < intersectingEdges.length; i++) {
+				for(j = 0; i < 2; j++) {
+					vertex = intersectingEdges[i][j];
+					if(vertex && plane.distanceToPoint(vertex) > 0)
+						edgeVerts[vertex] = vertex;
+				}
+			}
+
+			// Move each vertex to it's new location on the intersection of the sphere and the plane.
+			for(i in edgeVerts) {
+				var rotationAxis = (new THREE.Vector3()).copy(plane.normal).dot(edgeVerts[i]);
+				var y = plane.constant / sphere.radius;
+				var angleFromNorm = Math.asin(y);
+				var q = (new THREE.Quaternion()).fromAxisAngle(rotationAxis, angleFromNorm);
+				var position = (new THREE.Vector3()).copy(plane.normal).applyQuaternion(q);
+				edgeVerts[i].copy(position);
+			}
 
 			geometry.verticesNeedUpdate = true;
 			geometry.colorsNeedUpdate = true;
